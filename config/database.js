bf5f48088f13857cb0f52011754de1fedc46342a
lollyy28 <jennychristine2806@gmail.com>
2025-10-30 11:43:46 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,18 +1,15 @@
-import mongoose, { syncIndexes } from "mongoose";
+import mongoose from "mongoose"
 
 export const database = async () => {
     try {
         console.log("Starting connection to mongodb...");
 
-        const response = await mongoose.connect("mongodb://127.0.0.1:27017/merisa?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
+        const response = await mongoose.connect("mongodb://127.0.0.1:27017/Jenny?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
 
         console.log(`Connected succesfully to mongodb at : ${response.connection.host}`);
-
     } catch (error) {
         console.log("Mongodb connection failed");
 
         process.exit(1)
     }
 }
-
-export default database;
