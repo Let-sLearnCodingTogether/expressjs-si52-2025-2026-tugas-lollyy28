8d1fab28c2121c909d161ed718f4e1c831635d95
lollyy28 <nelson.marc5678@gmail.com>
2025-10-30 11:43:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import express from "express"
-import api from "./route/api.js";
-import database from "./config/database.js";
+import api from "./route/api.js"
+import { database } from "./config/database.js"
 
 import passport from "passport"
 import "./config/passport.js"
 
-const app = express();
+const app = express ()
+
 app.use(express.json())
 
 app.use(passport.initialize())
 
-app.use(express.static('public'))
+app.use(express.static('public'))   
 
 app.set('view engine', 'ejs')
 
@@ -18,5 +19,5 @@ app.use("/api", api)
 
 app.listen(3000, () => {
     database()
-    console.log(`Aplikasi berjalan di http://localhost:3000`);
+    console.log('Aplikasi berjalan di http://localhost:3000');
 })
